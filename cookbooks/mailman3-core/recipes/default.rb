7cd4e561ec73e0474a8373314799317ceae82204
saleem <meelash@gmail.com>
2020-08-01 21:10:28 +0100

    install python, pip, django

--- cookbooks/mailman3-core/recipes/default.rb
+++ cookbooks/mailman3-core/recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+
+include_recipe 'build-essential'
+
+# Install base packages for mailman3
+%w{
+  bzr
+  postfix
+  ssl-cert
+  python3-venv
+  python3-pip
+  python3-django
+  }.each do |pkg|
+    package pkg
+  end
